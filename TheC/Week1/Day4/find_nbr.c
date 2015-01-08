81e361a91bfda651a0d2df799cd75940e2f6a3b4
dcuevas94 <dcuevas@hawk.iit.edu>
2015-01-08 17:22:13 -0600

    another long day at work. so little free time to do this. literally like 2 mintues.

---
+++ TheC/Week1/Day4/find_nbr.c
@@ -0,0 +1,18 @@
+/* Name:		find_nbr.c
+ * Purpose:		Picks a random number and lets the user try to guess it
+ * Returns:		Nothing :P
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NO 0
+#define YES 1
+
+int main(void)
+{
+	int guess_value = -1;
+	int number;
+	int  nbrOfGuesses;
+}
