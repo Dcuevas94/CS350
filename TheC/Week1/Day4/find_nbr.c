42761030ef93adc56376507c681e14f5e811666b
dcuevas94 <dcuevas@hawk.iit.edu>
2015-01-09 12:56:20 -0600

    This book has some serious issues, who does these things?

--- TheC/Week1/Day4/find_nbr.c
+++ TheC/Week1/Day4/find_nbr.c
@@ -12,7 +12,39 @@
 
 int main(void)
 {
-	int guess_value = -1;
+	int guess_value = 0;
 	int number;
-	int  nbrOfGuesses;
+	int nbrOfGuesses;
+	int done = 0;
+
+	printf("\n\nGetting a random number!\n");
+	//Use the time to seed the random number generator
+	srand((unsigned) time (NULL)/500);
+	number = rand();
+	nbrOfGuesses = 0;
+	while(guess_value != number)
+	{
+		printf("%d",number);
+		printf("\n%d Pick a number between 0 and %d > ", nbrOfGuesses, RAND_MAX);
+		scanf("%d", &guess_value); //store the number
+			
+		nbrOfGuesses++;
+		if( number == guess_value)
+		{
+			printf("You got it!");
+			
+		}
+		else
+		if (number < guess_value)
+		{
+			printf("\nYou guessed too high!");
+		}
+		else
+		{
+			printf("\nYou guessed too low!");
+		}
+	}
+	printf("\n\nCongrats you guess right in %d Guesses!", nbrOfGuesses);
+	printf("\n\nTHe number was: %d\n\n", number);
+	return 0;
 }
